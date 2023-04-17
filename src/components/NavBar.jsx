cb03729c1c54f333c682299c3a89bb164a0237a4
Quan Pham <quandool0104@gmail.com>
2023-04-17 00:11:09 -0500

    Style Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar() {
+	return (
+		<>
+			<nav className="bg-black text-center py-3">
+				<h1 className="text-green-300 text-3xl font-bold">Jammming</h1>
+			</nav>
+		</>
+	);
+}
+
+export default NavBar;
