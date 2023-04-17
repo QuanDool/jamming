82d886204a41918376c003aa409f5083b117d749
Quan Pham <quandool0104@gmail.com>
2023-04-17 15:21:51 -0500

    Add Tracklist to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import SearchBar from "./components/SearchBar";
+import Tracklist from "./components/Tracklist";
 
 function App() {
 	return (
 		<>
 			<NavBar />
 			<SearchBar />
+			<Tracklist />
 		</>
 	);
 }
--- src/components/Tracklist.jsx
+++ src/components/Tracklist.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import SearchResults from "./SearchResults";
+import Playlist from "./Playlist";
 
 function Tracklist() {
 	return (
 		<>
-			<div>Tracklist</div>
+			<div className="flex flex-row flex-nowrap">
+				<SearchResults />
+				<Playlist />
+			</div>
 		</>
 	);
 }
